838897973e17ba6045c10c308be91f594cffde84
Romaric Richou <romaric.richou@ensta-paris.fr>
2021-05-02 15:13:47 +0200

    Programme complet de demande qui marche sur l'ordinateur de Romaric, changer wd pour un autre ordinateur

--- demand/consumption_script_to_fill.py
+++ demand/consumption_script_to_fill.py
@@ -1,4 +1,4 @@
-
+import demand as demand
 import pandas as pd
 import os
 from dfply import *
@@ -8,16 +8,17 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 #This function sets the working directory
-def set_wd(wd):
+def set_wd(wd="D:/cygwin64/home/roma5/project_folder/IN104-RICHOU_Romaric-Thomas_ZEREZ-TV/demand"):
     os.chdir(wd)
 
 #This function imports a csv file and has the option to plot its value columns as a function of the first column
 def import_csv(f_name = "DE.csv", delimeter = ";", plot = True):
-
-    return f >> mutate(Date = pd.to_datetime(conso['Date']))
+    f = pd.read_csv(f_name,delimeter)
+    return f >> mutate(Date = pd.to_datetime(f['Date (CET)']))
 
 #This function creates a scatter plot given a DataFrame and an x and y column
 def scatter_plot(dataframe = "conso", x = "Actual", y = "LDZ", col = "red"):
+    conso.plot(x,y,'scatter')
 
 #This function is the sigmoid function for gas consumption as a function of temperature
 def h(t, a, b, c, d):
@@ -36,6 +37,9 @@ def consumption_sigmoid(t, real_conso, a = 900, b = -35, c = 6, d = 300, plot = 
             if(len(t) != len(real_conso)):
                 print("Difference in length between Temperature and Real Consumption vectors")
             # add title and legend and show plot
+            plt.scatter(t,real_conso,label="Real conso")
+            plt.scatter(t,h_hat,label="Current model")
+            plt.show()
     return h_hat
 
 #The following function gets the fit metrics list between 2 sigmoids
@@ -43,36 +47,46 @@ def get_fit_metrics(h_hat, real_conso):
     if(len(h_hat) != len(real_conso)):
         print("Difference in length between Fit and Real Consumption vectors")
     else:
-        
-    return []
+        corr, _ = pearsonr(h_hat, real_conso)
+        rmse = np.sqrt(mean_squared_error(h_hat, real_conso))
+        nrmse = rmse / (np.max(real_conso) - np.min(real_conso))
+        anrmse = rmse / np.mean(real_conso)
+    return [corr,rmse,nrmse,anrmse]
 
 #The following class is the cosumption class it takes sigmoid parameters as well as a temperature as input
 class consumption:
     #Initialize class
     def __init__(self, a, b, c, d):
-        
+        self.a=a
+        self.b=b
+        self.c=c
+        self.d=d
 
     #calculate the consumption given a temperature
     def get_consumption(self, temperature):
-        
+        return(h(temperature,self.a,self.b,self.c,self.d))
 
     #get the sigmoid considering a temperature between -40 and 39, use the function consumption_sigmoid above
-    def sigmoid(self, p):
+    def sigmoid(self):
+        x=np.arange(-40,39,0.1)
+        plt.plot(x,self.get_consumption(x))
 
     #This is what the class print if you use the print function on it
     def __str__(self):
-        
-        return t
+        return "a="+str(self.a)+" ; b=" + str(self.b)+" ; c="+str(self.c)+" ; d="+str(self.d)
 
 #The following class optimizes the parameters of the sigmoid and returns an object of class consumption
 class optimize_sigmoid:
     #Initialize guess values that are common to all instances of the clasee
-    __guess_a, __guess_b, __guess_c, __guess_d
+    guess_a= 900
+    guess_b=-35
+    guess_c=6
+    guess_d=300
 
     def __init__(self, f):
         if isinstance(f, pd.DataFrame):
             if 'Actual' and 'LDZ' in f.columns:
-                
+                self.__f=f
             else:
                 print("Class not initialized since f does not contain Actual and LDZ column names")
         else:
@@ -81,15 +95,15 @@ class optimize_sigmoid:
     #optimize and return metrics use functions h, consumption_sigmoid defined above as well as get_fit_metrics
     def optimize(self):
         if self.__f is not None:
-            self.__coef, self.__cov = curve_fit(
-                h,
-
-                )
+            guess_a = 900
+            guess_b = -35
+            guess_c = 6
+            guess_d = 300
+            t= self.__f['Actual'].values
+            c= self.__f['LDZ'].values
+            self.__coef, self.__cov = curve_fit(h,t,c,[guess_a, guess_b, guess_c, guess_d])
             
-            s = consumption_sigmoid(
-
-                plot = True
-                )
+            s = consumption_sigmoid(t,c,self.__coef[0],self.__coef[1],self.__coef[2],self.__coef[3],plot = True)
             
             self.__corr, self.__rmse, self.__nrmse, self.__anrmse = get_fit_metrics(s, self.__f['LDZ'])
         else:
@@ -97,22 +111,22 @@ class optimize_sigmoid:
 
     #this function returns the fit metrics calculated above
     def fit_metrics(self):
-        if  is not None:
-            return 
+        if self.__corr is not None:
+            return [self.__coef[0],self.__coef[1],self.__coef[2],self.__coef[3]]
         else:
             print("optimize method is not yet run")
 
     #This function creates the class consumption
     def create_consumption(self):
-        if  is not None:
-            return 
+        if self.__corr is not None:
+            return consumption(self.__coef[0],self.__coef[1],self.__coef[2],self.__coef[3])
         else:
             print("optimize method is not yet run")
 
     #This is what the class print if you use the print function on it
     def __str__(self):
-        if is not None:
-
+        if self.__corr is not None:
+            t= "a="+str(self.__coef[0])+" ; b=" + str(self.__coef[1])+" ; c="+str(self.__coef[2])+" ; d="+str(self.__coef[3])
         else:
             t = "optimize method is not yet run"
         return t
@@ -126,10 +140,16 @@ if __name__ == '__main__':
     #1) import consumption data and plot it
     conso = import_csv()
 
+    #1)1. Plot LDZ, Actual and Normal as Date fonction + comments
+    conso.plot('Date', ['LDZ','Actual','Normal'],subplots=True)
+
+    #There is a lot of volatility even if we can see annual periodic evolution
+
     #2) work on consumption data (non-linear regression)
     #2)1. Plot consumption as a function of temperature    
 
-    scatter_plot()        
+    scatter_plot()
+    # The function looks like our model
 
     #2)2. optimize the parameters
     sig = optimize_sigmoid(conso)
@@ -172,7 +192,7 @@ if __name__ == '__main__':
         )
     
     print(sig.fit_metrics())
-    c.sigmoid(True)
+    c.sigmoid()
     print(c)
     
     #3) If time allows do TSA on actual temperature
