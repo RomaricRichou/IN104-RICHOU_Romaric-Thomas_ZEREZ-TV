44f099204df50f7645b0dd71ffcba19f1c2c148a
Thomas Zerez <Thomas.zerez@ensta-paris.fr>
2021-05-04 17:28:43 +0200

    modifications

--- supply/supply.py
+++ supply/supply.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 supply_1 = {}
 supply_2 = {}
 
+
+
 #This function sets the working directory
 def set_wd(wd="/home/thomas/Documents/IN104/Projet_IN104/IN104-RICHOU_Romaric-Thomas_ZEREZ-TV/supply/"):
     os.chdir(wd)
@@ -19,12 +21,22 @@ def import_excel(f_name = "storage_data.xlsx"):
     f = pd.read_excel(f_name)
     return f >> mutate(Date = pd.to_datetime(f['gasDayStartedOn']))
 
-
+# Importation des informations de prix
+def import_csv(f_name = "price_data.csv"):
+    f = pd.read_csv(f_name, ';')
+    return f >> mutate(Date = pd.to_datetime(f['Date']))
 
 if __name__ == '__main__':
 
     #set working directory
     set_wd()
 
-    #import supply data
-    supply_data = import_excel()
+    #import storage data
+    storage_data = import_excel()
+    # print(supply_data)
+    #import price data
+    price_data = import_csv()
+    # print(price_data)
+
+    supply_data = pd.merge(storage_data, price_data, how = 'inner', on= 'Date')
+    print(supply_data)
