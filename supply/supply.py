9565f4c11328e2785d958b597d43bc8ae8999e4c
Thomas Zerez <Thomas.zerez@ensta-paris.fr>
2021-05-04 16:30:50 +0200

    creating supply.py

---
+++ supply/supply.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+from dfply import *
+from scipy.optimize import curve_fit
+from scipy.stats import pearsonr
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
