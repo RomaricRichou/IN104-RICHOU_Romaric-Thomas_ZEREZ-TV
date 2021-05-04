2aef6d9521a3897f2614aac886428ab97bd67046
Thomas Zerez <Thomas.zerez@ensta-paris.fr>
2021-05-04 16:55:35 +0200

    modification de supply.py

--- supply/supply.py
+++ supply/supply.py
@@ -6,3 +6,25 @@ from scipy.stats import pearsonr
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
+# Création des deux dictionnaires vides
+supply_1 = {}
+supply_2 = {}
+
+#This function sets the working directory
+def set_wd(wd="/home/thomas/Documents/IN104/Projet_IN104/IN104-RICHOU_Romaric-Thomas_ZEREZ-TV/supply/"):
+    os.chdir(wd)
+
+# Importation des informations de storage
+def import_excel(f_name = "storage_data.xlsx"):
+    f = pd.read_excel(f_name)
+    return f >> mutate(Date = pd.to_datetime(f['gasDayStartedOn']))
+
+
+
+if __name__ == '__main__':
+
+    #set working directory
+    set_wd()
+
+    #import supply data
+    supply_data = import_excel()
