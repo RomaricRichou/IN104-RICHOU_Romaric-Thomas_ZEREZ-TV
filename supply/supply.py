e6a22f226921b8c7709baf43e9243bcda6a3060a
Thomas Zerez <Thomas.zerez@ensta-paris.fr>
2021-05-18 15:30:19 +0200

    Modifications de supply.py

--- supply/supply.py
+++ supply/supply.py
@@ -13,6 +13,8 @@ from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from sklearn.metrics import mean_squared_error
+import math
 
 # Création des dictionnaires vides
 supply_data = {}
@@ -38,15 +40,24 @@ def import_csv(f_name = "price_data.csv"):
     f = pd.read_csv(f_name, ';')
     return f >> mutate(Date = pd.to_datetime(f['Date']))
 
+
 # regressions
-def reglineaire(x, y):
+def reglineaire(x, y, indic = 1):
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
     lr = LogisticRegression()
     lr.fit(x_train,y_train)
     y_pred = lr.predict(x_test)
     cm = confusion_matrix(y_test, y_pred)
     probs = np.transpose(lr.predict_proba(x_test))[0]
-    return {'recall': metrics.recall_score(y_test, y_pred), 'neg_recall': cm[1,1]/(cm[0,1] + cm[1,1]), 'confusion': cm, 'precision': metrics.precision_score(y_test, y_pred), 'neg_precision':cm[1,1]/cm.sum(axis=1)[1], 'roc': metrics.roc_auc_score(y_test, probs), 'class_mod': "the logistic regression"}
+    if indic == 0:
+        return {'recall': metrics.recall_score(y_test, y_pred), 'neg_recall': cm[1,1]/(cm[0,1] + cm[1,1]), 'confusion': cm, 'precision': metrics.precision_score(y_test, y_pred), 'neg_precision':cm[1,1]/cm.sum(axis=1)[1], 'roc': metrics.roc_auc_score(y_test, probs), 'class_mod': "the logistic regression"}
+    else:
+        corr, _ = pearsonr(y_pred, y_test)
+        rmse = np.sqrt(mean_squared_error(y_pred, y_test))
+        nrmse = rmse / (np.max(y_test) - np.min(y_test))
+        anrmse = rmse / np.mean(y_test)
+        return {'r2': metrics.r2_score(y_test, y_pred), 'rmse': rmse, 'nrmse': nrmse, 'anrmse': anrmse, 'cor': corr, 'l_reg': "the regression"}
+
 
 def randforest(x, y):
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
@@ -72,18 +83,22 @@ if __name__ == '__main__':
         supply_data[i] = pd.merge(storage_data[i], price_data, how = 'inner', on= 'Date')
         NW[i] = supply_data[i].withdrawal - supply_data[i].injection
         NWB[i], LNW[i], FSW1[i], FSW2[i] = [], [], [], []
+        c = 0
         for j in range(len(NW[i])):
-            if j >= 1:
-                LNW[i].append(NW[i][j-1])
-            if NW[i][j] >= 0:
-                NWB[i].append(1)
-            else:
-                NWB[i].append(0)
-
-            FSW1[i].append(max(supply_data[i].full[j] - 45, 0))
-            FSW2[i].append(max(45 - supply_data[i].full[j], 0))
-        x = np.transpose([LNW[i][0:1602], FSW1[i][0:1602], FSW2[i][0:1602], price_data.SAS_GPL.values[0:1602], price_data.SAS_NBP.values[0:1602], price_data.SAS_NCG.values[0:1602], price_data.SAS_TTF.values[0:1602]])
-        y = NWB[i][0:1602]
+            if math.isfinite(supply_data[i].full[j]):
+                if NW[i][j] >= 0:
+                    c += 1
+                    if j >= 1:
+                        LNW[i].append(NW[i][j-1])
+                    # if NW[i][j] >= 0:
+                    #     NWB[i].append(1)
+                    # else:
+                    #     NWB[i].append(0)
+                    FSW1[i].append(max(supply_data[i].full[j] - 45, 0))
+                    FSW2[i].append(max(45 - supply_data[i].full[j], 0))
+        x = np.transpose([LNW[i][0:c-1], FSW1[i][0:c-1], FSW2[i][0:c-1], price_data.SAS_GPL.values[0:c-1], price_data.SAS_NBP.values[0:c-1], price_data.SAS_NCG.values[0:c-1], price_data.SAS_TTF.values[0:c-1]])
+        # y = NWB[i][0:1602]
+        y = NW[i][0:c-1]
         d[i] = reglineaire(x, y)
         d2[i] = randforest(x, y)
 
