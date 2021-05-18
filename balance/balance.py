7f177f58034d7d8c8a1223c67d61cb171c25a3ec
Thomas Zerez <Thomas.zerez@ensta-paris.fr>
2021-05-18 17:06:02 +0200

    creation de balance

---
+++ balance/balance.py
@@ -0,0 +1,7 @@
+import pickle
+
+## Importing supply data
+filename = '/home/thomas/Documents/IN104/Projet_IN104/IN104-RICHOU_Romaric-Thomas_ZEREZ-TV/supply/finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+
